612576a74ac51b711116e8644fdb0e652fd85e3c
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-13 16:13:22 +0800

    Create login and register pages

---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Add from "../images/photo-plus.png";
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Generic Chat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="Add avatar image" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>Do you have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
