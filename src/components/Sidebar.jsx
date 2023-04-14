ec92c22bb714d64bb82b1ed5ae79f14858c2db84
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-14 09:42:50 +0800

    Refactor sidebar and update style
    Created a separate user component for the sidebar
    Added button hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Search from "./Search";
 import Chats from "./Chats";
+import User from "./User";
 
 const Sidebar = () => {
   return (
@@ -9,6 +10,7 @@ const Sidebar = () => {
       <Navbar />
       <Search />
       <Chats />
+      <User />
     </div>
   );
 };
---
+++ src/components/User.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div className="user">
+      <button>Log out</button>
+      {/* Placeholder image */}
+      <img
+        src="https://howtodrawforkids.com/wp-content/uploads/2017/03/how-to-draw-a-face-step-by-step.jpg"
+        alt=""
+      />
+      <span>Sian Caleb</span>
+    </div>
+  );
+};
+
+export default User;
