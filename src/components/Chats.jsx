5c883ea9de51d32f2f9707f3e514ddfc1ccbda5f
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-14 20:47:33 +0800

    Add selected user to chats list

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { doc, onSnapshot } from "@firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
 
 const Chats = () => {
+  // chats[0] holds key while chats[1] holds the content
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  console.log("wehoo", Object.entries(chats));
+
+  // return <span>yeah</span>;
+
   return (
     <div className="chats">
-      <div className="userChat">
-        {/* Placeholder image */}
-        <img
-          src="https://howtodrawforkids.com/wp-content/uploads/2017/03/how-to-draw-a-face-step-by-step.jpg"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Caleb</span>
-          <p>This is the latest message</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
