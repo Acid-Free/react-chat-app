a3a52ca47433cf90c5fc90fa2e4dc6f20d74ef6c
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-14 21:06:35 +0800

    Create chat context

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,11 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { db } from "../firebase";
 import { doc, onSnapshot } from "@firebase/firestore";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
   // chats[0] holds key while chats[1] holds the content
   const [chats, setChats] = useState([]);
+
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -22,14 +25,18 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  console.log("wehoo", Object.entries(chats));
-
-  // return <span>yeah</span>;
+  const handleSelect = (user) => {
+    dispatch({ type: "CHANGE_USER", payload: user });
+  };
 
   return (
     <div className="chats">
       {Object.entries(chats)?.map((chat) => (
-        <div className="userChat" key={chat[0]}>
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
