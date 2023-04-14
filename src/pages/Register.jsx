04e77449a64e1218ff037c0fc3b6bd4864a6ee04
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-15 00:04:04 +0800

    Separate register logic from component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,9 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, db, storage } from "../firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore";
+// Register.js
+
 import React, { useState } from "react";
 import AddIcon from "../images/photo-plus.png";
 import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../registerUser";
 
 const Register = () => {
   const [error, setError] = useState("");
@@ -15,63 +14,20 @@ const Register = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    setUploading(true);
-
     const displayName = event.target[0].value;
     const email = event.target[1].value;
     const password = event.target[2].value;
     const file = event.target[3].files[0];
 
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-
-      // Uses current datetime and username for referencing user image
-      const dateTime = new Date().getTime();
-      const storageRef = ref(storage, `${dateTime + displayName}`);
-
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        (error) => {
-          console.error(error);
-          setError(error.code);
-          setUploading(false);
-        },
-        async () => {
-          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-
-          // Add updated name and avatar URL to user data in auth
-          await updateProfile(response.user, {
-            displayName,
-            photoURL: downloadURL,
-          });
-
-          // Save user information to users collection (this is separate from authentication db)
-          await setDoc(doc(db, "users", response.user.uid), {
-            uid: response.user.uid,
-            displayName,
-            email,
-            photoURL: downloadURL,
-          });
-
-          // Create a document for a list of people a user interacted to
-          await setDoc(doc(db, "userChats", response.user.uid), {});
-
-          // Redirect to home page after successful registration
-          navigate("/");
-        }
-      );
-    } catch (error) {
-      console.error(error);
-      setError(error.code);
-      setUploading(false);
-      await auth.currentUser.delete();
-      console.log("User account revoked successfully");
-    }
+    await registerUser(
+      displayName,
+      email,
+      password,
+      file,
+      navigate,
+      setError,
+      setUploading
+    );
   };
 
   return (
