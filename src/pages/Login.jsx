83ce84ddd5d62fbfdd4429a99e2cf53ea1b7d675
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-14 17:12:44 +0800

    Add login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const email = event.target[0].value;
+    const password = event.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      // Redirect to home page after successful sign in
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Generic Chat</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <button>Log in</button>
+          {error && <span>Something went wrong.</span>}
         </form>
         <p>Don't have an account? Register</p>
       </div>
