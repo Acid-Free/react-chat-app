13ac2ef48a9563367c8d70282462c20838695f28
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-14 22:22:38 +0800

    Add scrolling to latest message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
 
   return (
     <div
