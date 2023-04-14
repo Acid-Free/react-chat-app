795a1a2bfdfb042c1c445cb660d6abed8460e366
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-04-14 17:01:34 +0800

    Add protected route for index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./style.scss";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
@@ -11,11 +11,25 @@ function App() {
 
   console.log("Context:", currentUser);
 
+  // Prevents unauthorized routing to index
+  const ProtectedRoute = () => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="Register" element={<Register />} />
         </Route>
